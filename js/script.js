c67c4b68cfe20b7b338cedd89c968f58fc6b3c5f
rynnxz <119603477+rynnxz@users.noreply.github.com>
2023-12-27 16:58:00 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+    distance: '26px',
+    duration: 2600,
+    delat: 250,
+    reset: true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
